7bc30dbcdd3fa7a3e878e844f641088983343b3d
jmona789 <jmona789@gmail.com>
2015-11-18 20:36:05 -0500

    basic html, css, bootswacth and js for delting items

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".btn-danger").on("click", function(){
+    $(this).parent().parent().remove()
+  });
+  $(".btn-primary").on("click", function(){
+
+    $
+  });
+});
