d8072531e0d57ceb4c937f8d5691efe4d65c28dc
jmona789 <jmona789@gmail.com>
2015-11-19 10:45:10 -0500

    added onclick functions for add button, delete buttons, and checkboxes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
-  $(".btn-danger").on("click", function(){
+  $(document).on("click", ".btn-danger", function(){
     $(this).parent().parent().remove()
   });
-  $(".btn-primary").on("click", function(){
-
-    $
+  $(".btn-primary").on("click", function(e){
+    e.preventDefault();
+    var newItem = $("#newItem").val().trim();
+    var newRow = $("<tr>");
+    var checkbox = $("<input>").attr("type", "checkbox").attr("class", "checkboxes").attr("data-state", "not-checked");
+    var deleteBtn = $("<button>").addClass("btn btn-danger btn-xs").append("Delete");
+    var itemTd = $("<td>").addClass("word-td").append(newItem).append(deleteBtn).prepend(checkbox);
+    newRow.append(itemTd)
+    $("#listTable").append(newRow);
+    $("#newItem").val("").focus();
+  });
+  $(document).on("click", ".checkboxes", function(){
+    var dataState = $(this).attr("data-state")
+    if(dataState === "not-checked"){
+    $(this).parent().attr("class", "str")
+    $(this).attr("data-state", "checked")
+    }
+    else{
+    $(this).parent().attr("class", "null")
+    $(this).attr("data-state", "not-checked")
+    }
   });
 });
